690a6888dbe3b980da9775e08c27fd9ac8197aba
José Manuel Nieto Sánchez <superjmn@outlook.com>
2021-12-08 15:20:11 +0100

    Add ProportionalCanvas

---
+++ Zafiro.Avalonia/RectMixin.cs
@@ -0,0 +1,14 @@
+﻿using System.Numerics;
+using Avalonia;
+
+namespace Zafiro.Avalonia;
+
+public static class RectMixin
+{
+    public static Rect Multiply(this Rect rect, Vector2 multiplier)
+    {
+        var position = new Point(rect.X * multiplier.X, rect.Y * multiplier.Y);
+        var size = new Size(rect.Width * multiplier.X, rect.Height * multiplier.Y);
+        return new Rect(position, size);
+    }
+}
