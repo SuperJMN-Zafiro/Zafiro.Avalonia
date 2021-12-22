a5ca393e91ccfadee525b11c1b1fc98ded087105
José Manuel Nieto Sánchez <superjmn@outlook.com>
2021-12-22 22:27:49 +0100

    Un poquito de observable

---
+++ Zafiro.LibVLCSharp.Avalonia/ObservableVlcMediaPlayer.cs
@@ -0,0 +1,71 @@
+﻿using System.Reactive.Linq;
+using Avalonia.Platform;
+using LibVLCSharp.Shared;
+
+namespace Zafiro.Avalonia.LibVLCSharp
+{
+    public interface IVlcPlayer
+    {
+        public Media Media { get; set; }
+    }
+
+    public class VlcMediaPlayer : IDisposable
+    {
+        private readonly MediaPlayer mediaPlayer;
+        private IPlatformHandle handle;
+
+        public VlcMediaPlayer(MediaPlayer mediaPlayer)
+        {
+            this.mediaPlayer = mediaPlayer;
+            LengthChanged = Observable
+                .FromEventPattern<MediaPlayerLengthChangedEventArgs>(h => mediaPlayer.LengthChanged += h,
+                    h => mediaPlayer.LengthChanged -= h)
+                .Select(a => TimeSpan.FromMilliseconds(a.EventArgs.Length));
+
+            PositionChanged = Observable
+                .FromEventPattern<MediaPlayerTimeChangedEventArgs>(h => mediaPlayer.TimeChanged += h,
+                    h => mediaPlayer.TimeChanged -= h)
+                .Select(a => TimeSpan.FromMilliseconds(a.EventArgs.Time));
+        }
+
+        public IObservable<TimeSpan> PositionChanged { get; }
+
+        public IObservable<TimeSpan> LengthChanged { get; }
+
+        public IPlatformHandle Handle
+        {
+            get => handle;
+            set
+            {
+                handle = value;
+                mediaPlayer.SetHandle(value);
+            }
+        }
+
+        public void Play()
+        {
+            mediaPlayer.Play();
+        }
+
+        public void Dispose()
+        {
+            mediaPlayer.DisposeHandle();
+            mediaPlayer.Dispose();
+        }
+
+        public void SeekTo(TimeSpan pos)
+        {
+            mediaPlayer.SeekTo(pos);
+        }
+
+        public void Pause()
+        {
+            mediaPlayer.Pause();
+        }
+
+        public void Stop()
+        {
+            mediaPlayer.Stop();
+        }
+    }
+}
---
+++ Zafiro.LibVLCSharp.Avalonia/StringToVideoSourceConverter.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+using Avalonia.Data;
+using Avalonia.Data.Converters;
+using CSharpFunctionalExtensions;
+
+namespace Zafiro.Avalonia.LibVLCSharp;
+
+public class StringToVideoSourceConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is null)
+        {
+            return Maybe<VideoSource>.None;
+        }
+
+        if (value is string str)
+        {
+            return Maybe.From(new VideoSource(str));
+        }
+
+        return BindingValue<VideoSource>.DoNothing;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
+
