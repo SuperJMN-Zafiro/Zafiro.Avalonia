ca75a630f8a34a9632e1ccea27cb780959f0bf65
José Manuel Nieto Sánchez <superjmn@outlook.com>
2021-12-25 23:06:35 +0100

    Evolution

---
+++ Zafiro.LibVLCSharp.Avalonia/NotifyDataContextBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+using Avalonia;
+using Avalonia.Xaml.Interactivity;
+using ReactiveUI;
+
+namespace Zafiro.Avalonia.LibVLCSharp
+{
+    public class NotifyDataContextBehavior : Behavior<VideoView>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            AssociatedObject.GetObservable(StyledElement.DataContextProperty).Subscribe(Update);
+        }
+
+        private void Update(object? dataContext)
+        {
+            if (dataContext is not null)
+            {
+                MessageBus.Current.SendMessage(new MediaPlayerCreated(AssociatedObject, dataContext));
+            }
+            else
+            {
+                Debug.WriteLine("DataContext is null in ViewViewAdvanced");
+            }
+        }
+    }
+}
