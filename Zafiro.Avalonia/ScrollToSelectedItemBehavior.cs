3affc230ecd1c6557daa524ad9fd2b384e500d4e
José Manuel Nieto <superjmn@outlook.com>
2022-09-13 10:13:06 +0200

    Add ScrollToSelectedItemBehavior

---
+++ Zafiro.Avalonia/ScrollToSelectedItemBehavior.cs
@@ -0,0 +1,39 @@
+﻿using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using Avalonia.Controls;
+using Avalonia.Xaml.Interactivity;
+using ReactiveUI;
+
+namespace Zafiro.Avalonia;
+
+public class ScrollToSelectedItemBehavior : Behavior<TreeDataGrid>
+{
+    private readonly CompositeDisposable disposables = new();
+
+    protected override void OnAttachedToVisualTree()
+    {
+        if (AssociatedObject is { SelectionInteraction: { } selection, RowSelection: { } rowSelection })
+        {
+            Observable.FromEventPattern(selection, nameof(selection.SelectionChanged))
+                .Select(_ => rowSelection.SelectedIndex.FirstOrDefault())
+                .WhereNotNull()
+                .Do(ScrollToItemIndex)
+                .Subscribe()
+                .DisposeWith(disposables);
+        }
+    }
+
+    protected override void OnDetachedFromVisualTree()
+    {
+        disposables.Dispose();
+        base.OnDetachedFromVisualTree();
+    }
+
+    private void ScrollToItemIndex(int index)
+    {
+        if (AssociatedObject is { RowsPresenter: { } rowsPresenter })
+        {
+            rowsPresenter.BringIntoView(index);
+        }
+    }
+}
