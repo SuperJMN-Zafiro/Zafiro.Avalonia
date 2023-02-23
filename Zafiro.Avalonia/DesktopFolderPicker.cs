63636189c4123474b11465be7d958ffefbbdf646
José Manuel Nieto <superjmn@outlook.com>
2023-02-23 20:29:47 +0100

    Folder picker

---
+++ Zafiro.Avalonia/DesktopFolderPicker.cs
@@ -0,0 +1,28 @@
+﻿using System.Reactive.Linq;
+using Avalonia.Controls;
+using CSharpFunctionalExtensions;
+using Zafiro.FileSystem;
+using Zafiro.UI;
+
+namespace Zafiro.Avalonia;
+
+public class DesktopFolderPicker : IFolderPicker
+{
+    private readonly Window parent;
+    private readonly IZafiroFileSystem fileSystem;
+
+    public DesktopFolderPicker(Window parent, IZafiroFileSystem fileSystem)
+    {
+        this.parent = parent;
+        this.fileSystem = fileSystem;
+    }
+
+    public IObservable<Result<IZafiroDirectory>> Pick()
+    {
+        var picker = new OpenFolderDialog();
+        return 
+            Observable
+                .FromAsync(() => picker.ShowAsync(parent))
+                .Select(path => fileSystem.GetDirectory(path));
+    }
+}
