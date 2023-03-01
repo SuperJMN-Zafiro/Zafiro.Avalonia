62fbe35cae7f6a057940a7ccbc6cf8d04f9451a3
José Manuel Nieto <superjmn@outlook.com>
2023-03-01 17:07:20 +0100

    Más cambios

---
+++ Zafiro.Avalonia/NotificationService.cs
@@ -0,0 +1,23 @@
+﻿using Avalonia.Controls.Notifications;
+
+namespace Zafiro.Avalonia;
+
+public interface INotificationService
+{
+    void ShowMessage(string message);
+}
+
+public class NotificationService : INotificationService
+{
+    private readonly IManagedNotificationManager managedNotification;
+
+    public NotificationService(IManagedNotificationManager managedNotification)
+    {
+        this.managedNotification = managedNotification;
+    }
+
+    public void ShowMessage(string message)
+    {
+        managedNotification.Show(message);
+    }
+}
--- Zafiro.Avalonia/SaveFilePicker.cs
+++ Zafiro.Avalonia/SaveFilePicker.cs
@@ -16,12 +16,14 @@ public class SaveFilePicker : ISaveFilePicker
         this.parent = parent;
     }
 
-    public IObservable<Maybe<IStorable?>> Pick(params FileTypeFilter[] filters)
+    public IObservable<Maybe<IStorable?>> Pick(string desiredName, string defaultExtension, params FileTypeFilter[] filters)
     {
         return Observable
             .FromAsync(() => parent.StorageProvider.SaveFilePickerAsync(new FilePickerSaveOptions
             {
                 FileTypeChoices = FilePicker.Map(filters),
+                DefaultExtension = defaultExtension,
+                SuggestedFileName = desiredName,
             }))
             .Select(file => Maybe.From(file is null ? null : (IStorable)new StorableWrapper(file)));
     }
