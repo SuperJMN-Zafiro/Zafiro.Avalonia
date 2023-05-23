e50b140da6da59a58005a3c3c95caec2f3e38be1
José Manuel Nieto <superjmn@outlook.com>
2023-05-23 12:57:55 +0200

    Recover functionality

---
+++ src/Zafiro.Avalonia/ApplicationExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Controls;
+using JetBrains.Annotations;
+
+namespace Zafiro.Avalonia;
+
+[PublicAPI]
+public static class ApplicationExtensions
+{
+    public static void Connect(this Application application, Func<Control> createMainView, Func<Control, object> createDataContext)
+    {
+        var mainView = createMainView();
+
+        switch (application.ApplicationLifetime)
+        {
+            case IClassicDesktopStyleApplicationLifetime desktop:
+            {
+                var window = new Window
+                {
+                    Content = mainView,
+                };
+
+                desktop.MainWindow = window;
+                break;
+            }
+            case ISingleViewApplicationLifetime singleViewPlatform:
+                singleViewPlatform.MainView = mainView;
+                break;
+        }
+
+        mainView.Loaded += (_, _) =>
+        {
+            mainView.DataContext = createDataContext(mainView);
+        };
+    }
+}
