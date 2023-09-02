47b74a4209dfbdc17a6276c7489f39c9a2fcef7d
José Manuel Nieto <superjmn@outlook.com>
2023-09-03 00:48:43 +0200

    Simplify

---
+++ src/Zafiro.Avalonia.Dialogs/DialogExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Reactive;
+using AvaloniaSyncer.ViewModels;
+using CSharpFunctionalExtensions;
+using JetBrains.Annotations;
+using ReactiveUI;
+
+namespace Zafiro.Avalonia.Dialogs;
+
+[PublicAPI]
+public static class DialogExtensions
+{
+    public static async Task<Maybe<TReturn>> Prompt<TViewModel, TReturn>(this IDialogService dialogService, string title, TViewModel instance, string okTitle, Func<TViewModel, ReactiveCommand<Unit, TReturn>> commandFactory)
+    {
+        Maybe<TReturn> result = Maybe<TReturn>.None;
+        IDisposable? subscription = null;
+        await dialogService.ShowDialog(instance, title, new OptionConfiguration<TViewModel>(okTitle, context =>
+        {
+            var command = commandFactory(instance);
+            subscription = command.Subscribe(x => result = x);
+            return command.Extend(_ => context.Window.Close());
+        }));
+
+        subscription!.Dispose();
+        return result;
+    }
+}
--- src/Zafiro.Avalonia.Dialogs/DialogService.cs
+++ src/Zafiro.Avalonia.Dialogs/DialogService.cs
@@ -1,50 +1,27 @@
-﻿using Avalonia.Controls;
-using Avalonia.Controls.ApplicationLifetimes;
+﻿using Avalonia.Controls.ApplicationLifetimes;
 using CSharpFunctionalExtensions;
+using JetBrains.Annotations;
 
 namespace Zafiro.Avalonia.Dialogs;
 
 public abstract class DialogService : IDialogService
 {
-    public object GetFinalContent(object content)
+    public abstract Task ShowDialog<T>(T viewModel, string title, params OptionConfiguration<T>[] options);
+
+    [PublicAPI]
+    public static IDialogService Create(IApplicationLifetime lifetime, IDictionary<Type, Type> viewModelToViewDictionary, Maybe<Action<ConfigureWindowContext>> configureWindow)
     {
-        var type = ViewModelToViewDictionary.TryFind(content.GetType());
-        var finalContent = type.Match(t =>
+        return lifetime switch
         {
-            var instance = Activator.CreateInstance(t) ?? $"Could not create instance of {t}";
-            if (instance is Control c)
-            {
-                c.DataContext = content;
-            }
-
-            return instance;
-        }, () => content);
-
-        return finalContent;
+            IClassicDesktopStyleApplicationLifetime => new ClassicDesktopDialogService(configureWindow),
+            ISingleViewApplicationLifetime { MainView: not null } singleView => new SingleViewDialogService(singleView.MainView),
+            ISingleViewApplicationLifetime { MainView: null } => throw new InvalidOperationException("Please, set MainView in the Application.Current.Lifetime before calling this method"),
+            _ => throw new ArgumentOutOfRangeException(nameof(lifetime))
+        };
     }
-
-    public DialogService(IReadOnlyDictionary<Type, Type> viewModelToViewDictionary)
-    {
-        ViewModelToViewDictionary = viewModelToViewDictionary;
-    }
-
-    protected IReadOnlyDictionary<Type, Type> ViewModelToViewDictionary { get; }
 
     protected static IEnumerable<Option> CreateOptions<T>(object viewModel, IWindow window, IEnumerable<OptionConfiguration<T>> options)
     {
         return options.Select(configuration => new Option(configuration.Title, configuration.Factory(new ActionContext<T>(window, (T) viewModel))));
-    }
-
-    public abstract Task ShowDialog<T>(T viewModel, string title, params OptionConfiguration<T>[] options);
-
-    public static IDialogService Create(IApplicationLifetime lifetime, IReadOnlyDictionary<Type, Type> viewModelToViewDictionary, Maybe<Action<ConfigureWindowContext>> configureWindow)
-    {
-        return lifetime switch
-        {
-            IClassicDesktopStyleApplicationLifetime => new ClassicDesktopDialogService(viewModelToViewDictionary, configureWindow),
-            ISingleViewApplicationLifetime { MainView: not null } singleView => new SingleViewDialogService(singleView.MainView, viewModelToViewDictionary),
-            ISingleViewApplicationLifetime { MainView: null } => throw new InvalidOperationException("Please, set MainView in the Application.Current.Lifetime before calling this method"),
-            _ => throw new ArgumentOutOfRangeException(nameof(lifetime))
-        };
     }
 }
