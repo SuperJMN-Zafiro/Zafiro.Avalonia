bde4ce7db4615eb018d66c433d9329b6aa1ddfa1
José Manuel Nieto <superjmn@outlook.com>
2023-09-11 18:01:58 +0200

    Add new NotificationDialog

--- src/Zafiro.Avalonia.Dialogs/NotificationDialog.cs
+++ src/Zafiro.Avalonia.Dialogs/NotificationDialog.cs
@@ -1,7 +1,5 @@
 ﻿using CSharpFunctionalExtensions;
 using JetBrains.Annotations;
-using ReactiveUI;
-using Zafiro.Avalonia.Dialogs.Obsolete;
 using Zafiro.UI;
 
 namespace Zafiro.Avalonia.Dialogs;
@@ -9,18 +7,17 @@ namespace Zafiro.Avalonia.Dialogs;
 [PublicAPI]
 public class NotificationDialog : INotificationService
 {
-    private readonly Obsolete.IDialogService dialogService;
+    private readonly IDialogService dialogService;
 
-    public NotificationDialog(Obsolete.IDialogService dialogService)
+    public NotificationDialog(IDialogService dialogService)
     {
         this.dialogService = dialogService;
     }
 
     public Task Show(string message, Maybe<string> title)
     {
-        var messageDialogViewModel = new MessageDialogViewModel(message);
-        var dismissText = "OK";
-        var optionConfiguration = new OptionConfiguration<MessageDialogViewModel>(dismissText, context => ReactiveCommand.Create(() => context.Closeable.Close()));
-        return dialogService.ShowDialog(messageDialogViewModel, title.GetValueOrDefault("Failure"), optionConfiguration);
+        return dialogService.ShowMessage(DismissText.GetValueOrDefault("Dismiss"), title.GetValueOrDefault(), message);
     }
+
+    public Maybe<string> DismissText { get; set; }
 }
