58cdb1b7956e12ea0a4678b1ac484aeea0eedff2
José Manuel Nieto <superjmn@outlook.com>
2023-09-28 00:42:58 +0200

    Improve Styles New converter (Opacity)

--- src/Zafiro.Avalonia/Converters/BoolOpacityConverters.cs
+++ src/Zafiro.Avalonia/Converters/BoolOpacityConverters.cs
@@ -7,6 +7,9 @@ public static class BoolOpacityConverters
     public static readonly IValueConverter BoolToOpacity =
         new FuncValueConverter<bool, double>(x => x ? 1.0 : 0.0);
 
+    public static readonly IValueConverter BoolToOpacityInverted =
+        new FuncValueConverter<bool, double>(x => x ? 0.0 : 1.0);
+
     public static readonly IValueConverter OpacityToBool =
         new FuncValueConverter<double, bool>(x => x > 0);
 }
