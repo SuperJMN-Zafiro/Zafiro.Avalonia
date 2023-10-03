703f0ea5cac28ce4ae7596da54db8b8e052f002b
José Manuel Nieto <superjmn@outlook.com>
2023-10-03 13:01:13 +0200

    Tracker

---
+++ src/Zafiro.Avalonia/Mixins/SelectionTracker.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reactive.Linq;
+using Avalonia.Controls.Selection;
+using DynamicData;
+
+namespace Zafiro.Avalonia.Mixins;
+
+public class SelectionTracker<T, TKey> where TKey : notnull
+{
+    public SelectionTracker(SelectionModel<T> selection, Func<T, TKey> selector)
+    {
+        var cache = new SourceCache<T, TKey>(selector);
+
+        var obs = Observable
+            .FromEventPattern<SelectionModelSelectionChangedEventArgs<T>>(handler => selection.SelectionChanged += handler, handler => selection.SelectionChanged -= handler);
+
+        obs
+            .Do(pattern => Sync(pattern.EventArgs, cache))
+            .Subscribe();
+
+        Changes = cache.Connect();
+    }
+
+    public IObservable<IChangeSet<T, TKey>> Changes { get; }
+
+    private void Sync(SelectionModelSelectionChangedEventArgs<T> pattern, SourceCache<T, TKey> sourceCache)
+    {
+        sourceCache.Edit(x =>
+        {
+            x.Remove(pattern.DeselectedItems!);
+            x.AddOrUpdate(pattern.SelectedItems!);
+        });
+    }
+}
