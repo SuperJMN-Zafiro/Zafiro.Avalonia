848350c3c1681093d507dabe475a17ecb6347a4f
José Manuel Nieto <superjmn@outlook.com>
2023-12-11 23:46:22 +0100

    Qué fresco todo

--- src/Zafiro.Avalonia/Mixins/SelectionTracker.cs
+++ src/Zafiro.Avalonia/Mixins/SelectionTracker.cs
@@ -1,34 +1,43 @@
-﻿using System;
+﻿using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using Avalonia.Controls.Selection;
 using DynamicData;
 
 namespace Zafiro.Avalonia.Mixins;
 
-public class SelectionTracker<T, TKey> where TKey : notnull
+public class SelectionTracker<T, TKey> : IDisposable where T : notnull where TKey : notnull
 {
+    private readonly CompositeDisposable disposable = new();
+
     public SelectionTracker(SelectionModel<T> selection, Func<T, TKey> selector)
     {
-        var cache = new SourceCache<T, TKey>(selector);
+        var cache = new SourceCache<T, TKey>(selector)
+            .DisposeWith(disposable);;
 
         var obs = Observable
             .FromEventPattern<SelectionModelSelectionChangedEventArgs<T>>(handler => selection.SelectionChanged += handler, handler => selection.SelectionChanged -= handler);
 
         obs
             .Do(pattern => Sync(pattern.EventArgs, cache))
-            .Subscribe();
+            .Subscribe()
+            .DisposeWith(disposable);
 
         Changes = cache.Connect();
     }
 
     public IObservable<IChangeSet<T, TKey>> Changes { get; }
 
-    private void Sync(SelectionModelSelectionChangedEventArgs<T> pattern, SourceCache<T, TKey> sourceCache)
+    private static void Sync(SelectionModelSelectionChangedEventArgs<T> pattern, SourceCache<T, TKey> sourceCache)
     {
         sourceCache.Edit(x =>
         {
             x.Remove(pattern.DeselectedItems!);
             x.AddOrUpdate(pattern.SelectedItems!);
         });
+    }
+
+    public void Dispose()
+    {
+        disposable.Dispose();
     }
 }
