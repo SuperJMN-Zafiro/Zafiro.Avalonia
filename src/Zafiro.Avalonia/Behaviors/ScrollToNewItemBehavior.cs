809cc9f6d7c30dee8a75974e5b1afbfa2c710601
José Manuel Nieto <superjmn@outlook.com>
2023-12-11 17:11:38 +0100

    Add ScrollToNewItemBehavior

---
+++ src/Zafiro.Avalonia/Behaviors/ScrollToNewItemBehavior.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Specialized;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using Avalonia.Controls;
+using Avalonia.VisualTree;
+using Avalonia.Xaml.Interactions.Custom;
+using DynamicData.Binding;
+using JetBrains.Annotations;
+using ReactiveUI;
+
+namespace Zafiro.Avalonia.Behaviors;
+
+[PublicAPI]
+public class ScrollToNewItemBehavior : AttachedToVisualTreeBehavior<ItemsControl>
+{
+    protected override void OnAttachedToVisualTree(CompositeDisposable disposable)
+    {
+        var itemCollectionChanges = this.WhenAnyValue(x => x.AssociatedObject, x => x.AssociatedObject!.DataContext, x => x.AssociatedObject!.Items, (associateControl, _, _) => associateControl)
+            .WhereNotNull()
+            .Select(x => x.Items as INotifyCollectionChanged)
+            .WhereNotNull()
+            .Select(incc => incc.ObserveCollectionChanges())
+            .Switch();
+
+        var newItem = itemCollectionChanges
+            .Where(pattern => pattern.EventArgs.Action == NotifyCollectionChangedAction.Add)
+            .Select(eventPattern => eventPattern.EventArgs.NewItems?.Cast<object>().FirstOrDefault())
+            .WhereNotNull();
+
+        newItem
+            .Do(ScrollTo)
+            .Subscribe()
+            .DisposeWith(disposable);
+    }
+
+    private void ScrollTo(object obj)
+    {
+        // Avalonia doesn't provide us with any method to scroll to a given item, so we just scroll to end.
+        var scrollViewer = AssociatedObject.FindDescendantOfType<ScrollViewer>() ?? AssociatedObject.FindAncestorOfType<ScrollViewer>() ?? throw new InvalidOperationException("We can't find any ScrollViewer we can scroll.");
+        scrollViewer.ScrollToEnd();
+    }
+}
