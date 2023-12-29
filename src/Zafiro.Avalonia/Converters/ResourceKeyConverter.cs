5c057f76b39a85a7a1be37a88753a2abcb9c34ad
José Manuel Nieto <superjmn@outlook.com>
2023-12-29 01:57:29 +0100

    ResourceKeyConverter

---
+++ src/Zafiro.Avalonia/Converters/ResourceKeyConverter.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Data;
+using Avalonia.Data.Converters;
+
+namespace Zafiro.Avalonia.Converters;
+
+public class ResourceKeyConverter : AvaloniaObject, IValueConverter
+{
+    public static readonly StyledProperty<ResourceDictionary> DictionaryProperty = AvaloniaProperty.Register<ResourceKeyConverter, ResourceDictionary>(nameof(Dictionary));
+
+    public ResourceDictionary Dictionary
+    {
+        get => GetValue(DictionaryProperty);
+        set => SetValue(DictionaryProperty, value);
+    }
+
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value is not null ? Dictionary[value] : BindingValueType.UnsetValue;
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) => throw new NotImplementedException();
+}
