989487b339ede692397c33d63f5801a88c23f484
José Manuel Nieto <superjmn@outlook.com>
2024-06-04 16:22:50 +0200

    Muchos cambios

---
+++ src/Zafiro.Avalonia.Dialogs/DesktopDialogService2.cs
@@ -0,0 +1,74 @@
+﻿using System.Reactive;
+using System.Reactive.Linq;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using CSharpFunctionalExtensions;
+using ReactiveUI;
+using Zafiro.UI;
+
+namespace Zafiro.Avalonia.Dialogs;
+
+public interface IDialog2
+{
+    public Task Show(object viewModel, string title, IObservable<bool> canSubmit);
+}
+
+public interface IHaveCommit
+{
+    IObservable<Unit> Commited { get; }
+}
+
+public class DesktopDialogService2 : IDialog2
+{
+    private readonly Maybe<Action<ConfigureWindowContext>> configureWindowAction;
+
+    public DesktopDialogService2(Maybe<Action<ConfigureWindowContext>> configureWindowAction)
+    {
+        this.configureWindowAction = configureWindowAction;
+    }
+
+    public Task Show(object viewModel, string title, IObservable<bool> canSubmit)
+    {
+        if (viewModel == null)
+        {
+            throw new ArgumentNullException(nameof(viewModel));
+        }
+
+        var window = new Window
+        {
+            Title = title,
+            WindowStartupLocation = WindowStartupLocation.CenterOwner,
+            CanResize = false, 
+            SizeToContent = SizeToContent.WidthAndHeight,
+            Icon = MainWindow.Icon,
+        };
+        
+        window.Content = new DialogViewContainer()
+        {
+            Classes = { "Desktop" },
+            Content = new DialogView
+            {
+                DataContext = new DialogViewModel(viewModel,
+                    new Option("OK", ReactiveCommand.Create(() => window.Close(), canSubmit)))
+            }
+        };
+
+#if DEBUG        
+        window.AttachDevTools();
+#endif
+        
+        return window.ShowDialog(MainWindow);
+    }
+
+    private static Action<ConfigureWindowContext> DefaultWindowConfigurator()
+    {
+        return context =>
+        {
+            context.ToConfigure.Width = context.Parent.Bounds.Width / 3;
+            context.ToConfigure.Height = context.Parent.Bounds.Width / 3;
+        };
+    }
+
+    private static Window MainWindow => ((IClassicDesktopStyleApplicationLifetime)Application.Current!.ApplicationLifetime!).MainWindow!;
+}
--- src/Zafiro.Avalonia.Dialogs/IDialogService.cs
+++ src/Zafiro.Avalonia.Dialogs/IDialogService.cs
@@ -4,5 +4,10 @@ namespace Zafiro.Avalonia.Dialogs;
 
 public interface IDialogService
 {
-    Task<Maybe<TResult>> ShowDialog<TViewModel, TResult>(TViewModel viewModel, string title, Func<TViewModel, IObservable<TResult>> results, Maybe<Action<ConfigureWindowContext>> configureWindowActionOverride, params OptionConfiguration<TViewModel, TResult>[] options) where TViewModel : UI.IResult<TResult>;
+    Task<Maybe<TResult>> ShowDialog<TViewModel, TResult>(
+        TViewModel viewModel,
+        string title, Func<TViewModel, IObservable<TResult>> results,
+        Maybe<Action<ConfigureWindowContext>> configureWindowActionOverride,
+        params OptionConfiguration<TViewModel, TResult>[] options)
+        where TViewModel : UI.IResult<TResult>;
 }
