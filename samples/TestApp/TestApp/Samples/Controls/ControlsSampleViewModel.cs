01231d1d365557550f0e02cc028fb148ef7ff089
José Manuel Nieto <superjmn@outlook.com>
2024-10-03 11:39:39 +0200

    Create random graph

--- samples/TestApp/TestApp/Samples/Controls/ControlsSampleViewModel.cs
+++ samples/TestApp/TestApp/Samples/Controls/ControlsSampleViewModel.cs
@@ -18,21 +18,10 @@ public class ControlsSampleViewModel
         Items = new List<Item> { new Item(1), new Item(2), new Item(3) };
         //SelectionHandler = new SelectionHandler<Item, int>(SelectionModel, arg => arg.Id);
 
-        var jmn = new Person("JMN");
-        var ana = new Person("Ana");
-        var pablo = new Person("Pablo");
-
-        var rel1 = new Friendship(jmn, ana, 1);
-        var rel2 = new Friendship(jmn, pablo, 2);
-        var rel3 = new Friendship(ana, pablo, 2);
-
-        var edges = new IEdge<INode2D>[] { rel1, rel2, rel3 };
-        var nodes = new List<INode2D> { jmn, ana, pablo };
-
-        var g = new RandomGraphGenerator().GenerateRandomGraph(40, 4);
+        var graph = new RandomGraphGenerator().GenerateRandomGraph(30, 3);
         
-        var ffd = new ForceDirectedGraph(new Graph2D(g.nodes.Cast<INode2D>().ToList(), g.edges.Cast<IEdge<INode2D>>().ToList()));
-        ffd.Distribute(1000, 1000);
+        var ffd = new ForceDirectedGraph(new Graph2D(graph.nodes.Cast<INode2D>().ToList(), graph.edges.Cast<IEdge<INode2D>>().ToList()));
+        ffd.Distribute(1900, 1300);
         Graph = new GraphWrapper(ffd);
 
         Play = ReactiveCommand
@@ -49,45 +38,4 @@ public class ControlsSampleViewModel
 
     public SelectionModel<Item> SelectionModel { get; }
     public IGraph Graph { get; }
-}
-
-public class RandomGraphGenerator
-{
-    private Random random = new Random();
-
-    public (List<Person> nodes, List<Friendship> edges) GenerateRandomGraph(int maxNodes, int maxEdgesPerNode)
-    {
-        var nodes = GenerateNodes(maxNodes);
-        var edges = GenerateEdges(nodes, maxEdgesPerNode);
-        return (nodes, edges);
-    }
-
-    private List<Person> GenerateNodes(int maxNodes)
-    {
-        var nodes = new List<Person>();
-        for (int i = 0; i < maxNodes; i++)
-        {
-            nodes.Add(new Person($"Person_{i}"));
-        }
-        return nodes;
-    }
-
-    private List<Friendship> GenerateEdges(List<Person> nodes, int maxEdgesPerNode)
-    {
-        var edges = new List<Friendship>();
-        foreach (var node in nodes)
-        {
-            var edgeCount = random.Next(0, maxEdgesPerNode + 1);
-            var potentialFriends = nodes.Where(n => n != node && !edges.Any(e => (e.Source== node && e.Target== n) || (e.Source== n && e.Target== node))).ToList();
-            
-            for (int i = 0; i < edgeCount && potentialFriends.Any(); i++)
-            {
-                var friend = potentialFriends[random.Next(potentialFriends.Count)];
-                var strength = random.Next(1, 300); // Asumiendo que la fuerza de la amistad es entre 1 y 3
-                edges.Add(new Friendship(node, friend, strength));
-                potentialFriends.Remove(friend);
-            }
-        }
-        return edges;
-    }
 }
---
+++ samples/TestApp/TestApp/Samples/Controls/RandomGraphGenerator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TestApp.Samples.Controls;
+
+public class RandomGraphGenerator
+{
+    private Random random = new Random();
+
+    public (List<Person> nodes, List<Friendship> edges) GenerateRandomGraph(int maxNodes, int maxEdgesPerNode)
+    {
+        var nodes = GenerateNodes(maxNodes);
+        var edges = GenerateEdges(nodes, maxEdgesPerNode);
+        return (nodes, edges);
+    }
+
+    private List<Person> GenerateNodes(int maxNodes)
+    {
+        var nodes = new List<Person>();
+        for (int i = 0; i < maxNodes; i++)
+        {
+            nodes.Add(new Person($"Person_{i}"));
+        }
+        return nodes;
+    }
+
+    private List<Friendship> GenerateEdges(List<Person> nodes, int maxEdgesPerNode)
+    {
+        var edges = new List<Friendship>();
+        foreach (var node in nodes)
+        {
+            var edgeCount = random.Next(0, maxEdgesPerNode + 1);
+            var potentialFriends = nodes.Where(n => n != node && !edges.Any(e => (e.Source== node && e.Target== n) || (e.Source== n && e.Target== node))).ToList();
+            
+            for (int i = 0; i < edgeCount && potentialFriends.Any(); i++)
+            {
+                var friend = potentialFriends[random.Next(potentialFriends.Count)];
+                var strength = random.Next(1, 300); 
+                edges.Add(new Friendship(node, friend, strength));
+                potentialFriends.Remove(friend);
+            }
+        }
+        return edges;
+    }
+}
