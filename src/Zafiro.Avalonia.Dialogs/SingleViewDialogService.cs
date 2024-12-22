46a2f681dbc08c0a1c666c5d50eb86aa94945d4b
José Manuel Nieto (SuperJMN) <superjmn@outlook.com>
2024-12-22 18:00:12 +0100

    Add AdornerDialog

--- src/Zafiro.Avalonia.Dialogs/SingleViewDialogService.cs
+++ src/Zafiro.Avalonia.Dialogs/SingleViewDialogService.cs
@@ -1,73 +1,100 @@
-﻿using System.Reactive;
+﻿using System.Diagnostics;
 using System.Reactive.Linq;
-using System.Reactive.Subjects;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
+using Avalonia.Controls.Templates;
 using Avalonia.Layout;
 using CSharpFunctionalExtensions;
 using ReactiveUI;
+using Zafiro.Avalonia.Dialogs;
 
-namespace Zafiro.Avalonia.Dialogs;
+public class AdornerDialog : IDialog, ICloseable
+{
+    private readonly AdornerLayer layer;
+    private readonly Stack<Control> dialogs = new();
+    private TaskCompletionSource<bool>? currentDialog;
 
-// public class SingleViewDialogService : IDialogService
-// {
-//     private readonly Stack<Control> dialogs = new();
-//     private readonly AdornerLayer layer;
-//     private readonly Subject<Unit> closeSubject = new();
-//
-//     public SingleViewDialogService(Visual control)
-//     {
-//         layer = AdornerLayer.GetAdornerLayer(control) ?? throw new InvalidOperationException($"Could not get Adorner Layer from {control}");
-//     }
-//
-//     public async Task<Maybe<TResult>> ShowDialog<TViewModel, TResult>(TViewModel viewModel, string title, Func<TViewModel, IObservable<TResult>> results, Maybe<Action<ConfigureSizeContext>> configureWindowActionOverride, params OptionConfiguration<TViewModel, TResult>[] options) where TViewModel : UI.IResult<TResult>
-//     {
-//         if (viewModel == null)
-//         {
-//             throw new ArgumentNullException(nameof(viewModel));
-//         }
-//
-//         var view = new DialogView
-//         {
-//             DataContext = new DialogViewModel(viewModel, options.Select(x => new Option(x.Title, command: x.Factory(viewModel))).ToArray())
-//         };
-//
-//         var dialog = new DialogViewContainer
-//         {
-//             Title = title,
-//             Classes = { "Mobile" },
-//             Close = ReactiveCommand.Create(() =>
-//             {
-//                 Close();
-//                 closeSubject.OnNext(Unit.Default);
-//             }),
-//             VerticalAlignment = VerticalAlignment.Stretch,
-//             HorizontalAlignment = HorizontalAlignment.Stretch,
-//             CornerRadius = new CornerRadius(10),
-//             Content = view,
-//             [!Layoutable.HeightProperty] = layer.Parent!.GetObservable(Visual.BoundsProperty).Select(rect => rect.Height).ToBinding(),
-//             [!Layoutable.WidthProperty] = layer.Parent!.GetObservable(Visual.BoundsProperty).Select(rect => rect.Width).ToBinding()
-//         };
-//
-//         layer.Children.Add(dialog);
-//         dialogs.Push(dialog);
-//
-//         var firstResult = results(viewModel)
-//             .Select(x => Maybe.From(x))
-//             .ObserveOn(SynchronizationContext.Current!)
-//             .Do(_ => Close());
-//
-//         var closeResult = closeSubject.Select(_ => Maybe<TResult>.None);
-//         var merged = firstResult.Merge(closeResult).FirstAsync();
-//         var result = await merged;
-//         
-//         return result;
-//     }
-//
-//     private void Close()
-//     {
-//         var valueTuple = dialogs.Pop();
-//         layer.Children.Remove(valueTuple);
-//     }
-// }
+    public AdornerDialog(Visual control, DataTemplates? dataTemplates = null)
+    {
+        layer = AdornerLayer.GetAdornerLayer(control) 
+            ?? throw new InvalidOperationException($"Could not get Adorner Layer from {control}");
+        DataTemplates = dataTemplates.AsMaybe();
+    }
+
+    public Maybe<DataTemplates> DataTemplates { get; }
+
+    public async Task<bool> Show(object viewModel, string title, Func<ICloseable, IEnumerable<IOption>> optionsFactory)
+    {
+        if (viewModel == null) throw new ArgumentNullException(nameof(viewModel));
+
+        currentDialog = new TaskCompletionSource<bool>();
+        var options = optionsFactory(this);
+
+        var content = new DialogView
+        {
+            Content = viewModel,
+            Options = options
+        };
+
+        content.DataTemplates.AddRange(GetDialogTemplates());
+
+        var dialog = new DialogViewContainer
+        {
+            Title = title,
+            Classes = { "Mobile" },
+            Content = content,
+            Close = ReactiveCommand.Create(() => Dismiss()),
+            VerticalAlignment = VerticalAlignment.Stretch,
+            HorizontalAlignment = HorizontalAlignment.Stretch,
+            CornerRadius = new CornerRadius(10)
+        };
+
+        dialog[!Layoutable.HeightProperty] = layer.Parent!
+            .GetObservable(Visual.BoundsProperty)
+            .Select(rect => rect.Height)
+            .ToBinding();
+
+        dialog[!Layoutable.WidthProperty] = layer.Parent!
+            .GetObservable(Visual.BoundsProperty)
+            .Select(rect => rect.Width)
+            .ToBinding();
+
+        layer.Children.Add(dialog);
+        dialogs.Push(dialog);
+
+        var result = await currentDialog.Task.ConfigureAwait(false);
+        return result;
+    }
+
+    private DataTemplates GetDialogTemplates()
+    {
+        var map = Application.Current.AsMaybe().Map(Dialog.GetTemplates);
+        var templates = DataTemplates.Or(map);
+        return templates.GetValueOrDefault(new DataTemplates());
+    }
+
+    public void Close()
+    {
+        if (dialogs.Count > 0)
+        {
+            var dialog = dialogs.Pop();
+            layer.Children.Remove(dialog);
+        }
+
+        currentDialog?.TrySetResult(true);
+        currentDialog = null;
+    }
+
+    public void Dismiss()
+    {
+        if (dialogs.Count > 0)
+        {
+            var dialog = dialogs.Pop();
+            layer.Children.Remove(dialog);
+        }
+
+        currentDialog?.TrySetResult(false);
+        currentDialog = null;
+    }
+}
