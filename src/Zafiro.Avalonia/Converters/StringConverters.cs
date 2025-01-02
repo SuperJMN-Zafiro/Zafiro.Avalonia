3a5463e0f4a13b40ced1ddd71c51580ac37d7981
José Manuel Nieto (SuperJMN) <superjmn@outlook.com>
2025-01-02 12:00:05 +0100

    Add new String converter

--- src/Zafiro.Avalonia/Converters/StringConverters.cs
+++ src/Zafiro.Avalonia/Converters/StringConverters.cs
@@ -10,4 +10,5 @@ public class StringConverters
     public static FuncMultiValueConverter<string, string> JoinWithSpaces { get; } = new(strings => { return strings.Select(s => s ?? "").JoinWith(" "); });
 
     public static FuncMultiValueConverter<object, string> String { get; } = new(obj => obj.FirstOrDefault()?.ToString() ?? string.Empty);
+    public static FuncValueConverter<object, string> AsString { get; } = new(obj => obj?.ToString() ?? string.Empty);
 };
