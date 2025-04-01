4f59a9e3f636eb79d7f24a7951a513a4a8b32659
SuperJMN <superjmn@outlook.com>
2025-04-01 16:42:56 +0200

    Parece que va mejor

--- samples/TestApp/TestApp/Samples/ControlsNew/ControlsViewModel.cs
+++ samples/TestApp/TestApp/Samples/ControlsNew/ControlsViewModel.cs
@@ -1,62 +1,64 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Microsoft.Extensions.DependencyInjection;
 using TestApp.Samples.ControlsNew.Navigation;
 using TestApp.Samples.ControlsNew.SlimDataGrid;
 using TestApp.Samples.ControlsNew.Typewriter;
 using TestApp.Samples.ControlsNew.Wizard;
 using Zafiro.Avalonia.Dialogs;
-using Zafiro.Avalonia.Shell;
 using Zafiro.Avalonia.Shell.Sections;
 using Zafiro.UI.Navigation;
 using Zafiro.UI.Navigation.Zafiro.UI.Navigation;
 using Section = Zafiro.Avalonia.Shell.Sections.Section;
 
-namespace TestApp.Samples.ControlsNew;
-
-public class ControlsViewModel
+namespace TestApp.Samples.ControlsNew
 {
-    public ControlsViewModel()
+    public class ControlsViewModel
     {
-        var serviceCollection = new ServiceCollection();
-        
-        // Register your view models first
-        serviceCollection.AddScoped<NavigationSampleViewModel>();
-        serviceCollection.AddScoped<IOtherDependency, OtherDependency>();
-        
-        // Build the initial provider
-        var provider = serviceCollection.BuildServiceProvider();
-        
-        // Create a ServiceProviderTypeResolver with parameter support
-        var typeResolver = new ServiceProviderTypeResolver(provider);
-        
-        // Register any custom factories if needed
-        // Example: typeResolver.RegisterFactory<DetailViewModel, string>((sp, id) => new DetailViewModel(id, sp.GetService<IOtherDependency>()));
-        typeResolver.RegisterFactory<TargetViewModel, string>((sp, id) => new TargetViewModel(id, sp.GetRequiredService<IOtherDependency>()));
-        
-        // Register the Navigator with the resolver
-        serviceCollection.AddScoped<INavigator>(_ => new Navigator(typeResolver));
-        
-        // Build the final provider with the Navigator
-        provider = serviceCollection.BuildServiceProvider();
-        
-        Sections = new List<SectionBase>
+        private readonly IServiceProvider serviceProvider;
+
+        public ControlsViewModel()
         {
-            Section.Create("Typewriter", () => new TypewriterViewModel()),
-            Section.Create("DataGrid", () => new SlimDataGridViewModel()),
-            Section.Create("Wizard", () => new WizardViewModel(DialogService.Create())),
-            CreateNavigation<NavigationSampleViewModel>("Navigation", provider),
-        };
+            var serviceCollection = new ServiceCollection();
+            
+            // Register the ServiceProviderTypeResolver factory
+            serviceCollection.AddScoped<ITypeResolver>(provider => 
+                new ServiceProviderTypeResolver(provider));
+                
+            // Register the Navigator with the resolver
+            serviceCollection.AddScoped<INavigator>(provider => 
+                new Navigator(provider.GetRequiredService<ITypeResolver>()));
+            
+            // Register your view models
+            serviceCollection.AddScoped<NavigationSampleViewModel>();
+            serviceCollection.AddScoped<TargetViewModel>();
+            
+            // Build the provider
+            serviceProvider = serviceCollection.BuildServiceProvider();
+            
+            Sections = new List<SectionBase>
+            {
+                Section.Create("Typewriter", () => new TypewriterViewModel()),
+                Section.Create("DataGrid", () => new SlimDataGridViewModel()),
+                Section.Create("Wizard", () => new WizardViewModel(DialogService.Create())),
+                CreateNavigation<NavigationSampleViewModel>("Navigation")
+            };
+        }
+
+        private SectionBase CreateNavigation<T>(string name) where T : notnull
+        {
+            return Section.Create(name, () => {
+                // Create a new NavigationHost with a scoped resolver
+                return new NavigationHost(
+                    // Factory to create a scoped resolver
+                    () => new ScopedTypeResolver(serviceProvider),
+                    
+                    // Factory to create initial content using the resolver
+                    resolver => resolver.Resolve<T>()
+                );
+            });
+        }
+
+        public List<SectionBase> Sections { get; }
     }
-
-    private SectionBase CreateNavigation<T>(string name, ServiceProvider provider) where T : notnull
-    {
-        return Section.Create(name, () => new NavigationHost(provider, serviceProvider => 
-            serviceProvider.GetRequiredService<T>()));
-    }
-
-    public List<SectionBase> Sections { get; }
-}
-
-public class OtherDependency : IOtherDependency
-{
 }
