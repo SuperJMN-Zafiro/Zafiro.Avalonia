0ad0badf88c7ca27870fc9eeda85457aa0e2e02d
SuperJMN <superjmn@outlook.com>
2025-04-16 12:36:01 +0200

    Improve Loading customization

--- src/Zafiro.Avalonia/Controls/Loading.axaml.cs
+++ src/Zafiro.Avalonia/Controls/Loading.axaml.cs
@@ -24,4 +24,22 @@ public class Loading : ContentControl
         get => GetValue(IsLoadingProperty);
         set => SetValue(IsLoadingProperty, value);
     }
+
+    public static readonly StyledProperty<string> LoadingTextProperty = AvaloniaProperty.Register<Loading, string>(
+        nameof(LoadingText), "Loading...");
+
+    public string LoadingText
+    {
+        get => GetValue(LoadingTextProperty);
+        set => SetValue(LoadingTextProperty, value);
+    }
+
+    public static readonly StyledProperty<double> ProgressIndicatorSizeProperty = AvaloniaProperty.Register<Loading, double>(
+        nameof(ProgressIndicatorSize), 64);
+
+    public double ProgressIndicatorSize
+    {
+        get => GetValue(ProgressIndicatorSizeProperty);
+        set => SetValue(ProgressIndicatorSizeProperty, value);
+    }
 }
