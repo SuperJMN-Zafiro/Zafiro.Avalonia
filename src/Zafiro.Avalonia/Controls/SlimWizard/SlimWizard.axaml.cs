8dfa9f122980c0ff50e61f15e5897c67c98e96b0
SuperJMN <superjmn@outlook.com>
2025-05-09 10:14:22 +0200

    Refactor wizard interface and commands for improved consistency
    Replaced `IWizard` with `INewWizard` and refactored related properties, commands, and bindings to align with the new structure. Updated `Page` to implement `IPage` and introduced more explicit property handling such as `Next` and `Back`. Adjusted usages and tests to reflect these changes.

--- src/Zafiro.Avalonia/Controls/SlimWizard/SlimWizard.axaml.cs
+++ src/Zafiro.Avalonia/Controls/SlimWizard/SlimWizard.axaml.cs
@@ -5,10 +5,10 @@ namespace Zafiro.Avalonia.Controls.SlimWizard;
 
 public class SlimWizard : TemplatedControl
 {
-    public static readonly StyledProperty<IWizard> WizardProperty = AvaloniaProperty.Register<SlimWizard, IWizard>(
+    public static readonly StyledProperty<INewWizard> WizardProperty = AvaloniaProperty.Register<SlimWizard, INewWizard>(
         nameof(Wizard));
 
-    public IWizard Wizard
+    public INewWizard Wizard
     {
         get => GetValue(WizardProperty);
         set => SetValue(WizardProperty, value);
--- src/Zafiro.Avalonia/Controls/SlimWizard/WizardDesign.cs
+++ src/Zafiro.Avalonia/Controls/SlimWizard/WizardDesign.cs
@@ -1,17 +1,19 @@
-using CSharpFunctionalExtensions;
 using Zafiro.UI.Commands;
 using Zafiro.UI.Wizards;
 
 namespace Zafiro.Avalonia.Controls.SlimWizard;
 
-public class WizardDesign : IWizard
+public class WizardDesign : INewWizard
 {
-    public int CurrentPageIndex { get; set; } = 1;
+    public IEnhancedCommand Next { get; } = ReactiveCommand.Create(() => { }).Enhance();
+    public IEnhancedCommand Back { get; } = ReactiveCommand.Create(() => { }).Enhance();
+    public IPage CurrentPage { get; } = new PageDesign();
     public int TotalPages { get; } = 3;
-    public object CurrentPage { get; set; } = new TextBlock() { Text = "Hello world!" };
-    public MaybeViewModel<string> CurrentTitle { get; set; } = new("This is the Title of the Current Content");
-    public IEnhancedUnitCommand BackCommand { get; } = EnhancedCommand.Enhance(ReactiveCommand.Create(() => { }));
-    public IEnhancedCommand<Result<object>> NextCommand { get; } = EnhancedCommand.Enhance(ReactiveCommand.Create(() => Result.Success<object>(123)));
-    public string NextText { get; } = "Next";
-    public IObservable<object?> Finished => Finished.Cast<object>();
+}
+
+public class PageDesign : IPage
+{
+    public object Content { get; } = "This is some content";
+    public string Title { get; } = "Title";
+    public int Index { get; } = 2;
 }
