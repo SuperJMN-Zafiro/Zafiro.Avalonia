eec06123aeec3a685d3318f0b3ab5225958b820d
SuperJMN <superjmn@outlook.com>
2025-05-30 17:14:02 +0200

    Update `ShellSplitViewSampleViewModel` section sort index
    - Changed `sortIndex` value from 12 to 1 for `ShellSplitView` section to prioritize its display order.

--- samples/TestApp/TestApp/Samples/Shell/ShellSplitViewSampleViewModel.cs
+++ samples/TestApp/TestApp/Samples/Shell/ShellSplitViewSampleViewModel.cs
@@ -2,5 +2,5 @@ using Zafiro.UI.Shell.Utils;
 
 namespace TestApp.Samples.Shell;
 
-[Section(name: "ShellSplitView", icon: "mdi-view-split-vertical", sortIndex: 12)]
+[Section(name: "ShellSplitView", icon: "mdi-view-split-vertical", sortIndex: 1)]
 public class ShellSplitViewSampleViewModel;
