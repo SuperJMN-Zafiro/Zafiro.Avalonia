90619a1aae0cd7388504b9ec4c2b5abc4e143215
José Manuel Nieto <superjmn@outlook.com>
2025-05-31 00:43:42 +0200

    Introduce GitHub release automation and improve build configurability (#81)
    * Introduce GitHub release automation and improve build configurability
    
    - Added `PublishGitHubRelease` target with support for Linux, Windows, and Android deployments.
    - Introduced `Configuration.cs` for build configuration management with Debug/Release modes.
    - Enhanced Android deployment with signing options for Base64-encoded keystore support.
    - Updated Zafiro and related package dependencies to align with latest versions.
    - Added `Zafiro.Nuke` dependency to streamline Nuke build actions.
    
    * Downgrade Nuke.Common package version and enhance build configuration
    
    - Rolled back `Nuke.Common` package to version 8.1.4 for compatibility.
    - Added `PublishGitHubRelease` target to build schema.
    - Introduced additional Android signing options including `AndroidSigningKeyAlias`, `Base64Keystore`, and more, enhancing flexibility for deployment.
    - Improved schema with `Configuration` property supporting Debug/Release modes.
    
    Update package versions in Directory.Packages.props
    
    - Bumped Nuke.Common to 9.0.4.
    - Bumped Xamarin.AndroidX.Core.SplashScreen to 1.0.1.15.
    - Bumped Zafiro.Nuke to 1.0.11.
    
    Update azure-pipelines.yml

--- build/Build.cs
+++ build/Build.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using CSharpFunctionalExtensions;
+using DotnetPackaging;
 using Nuke.Common;
 using Nuke.Common.Git;
 using Nuke.Common.ProjectModel;
@@ -10,16 +11,26 @@ using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Tools.GitVersion;
 using Zafiro.Deployment;
 using Zafiro.Misc;
+using Zafiro.Nuke;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
+using Options = DotnetPackaging.Options;
 
 class Build : NukeBuild
 {
+    // Android
+    [Parameter("The alias for the key in the keystore.")] readonly string AndroidSigningKeyAlias;
+    [Parameter("The password of the key within the keystore file.")] [Secret] readonly string AndroidSigningKeyPass;
+    [Parameter("The password for the keystore file.")] [Secret] readonly string AndroidSigningStorePass;
+    [Parameter("Contents of the keystore encoded as Base64.")] readonly string Base64Keystore;
+    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")] readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
     [Parameter] readonly bool Force;
     [Parameter("GitHub Authentication Token")] [Secret] readonly string GitHubApiKey;
     [GitVersion] readonly GitVersion GitVersion;
     [Parameter("NuGet Authentication Token")] [Secret] readonly string NuGetApiKey;
     [GitRepository] readonly GitRepository Repository;
     [Solution] readonly Solution Solution;
+
+    Actions Actions { get; set; }
 
     Target RestoreWorkloads => _ => _
         .Executes(() =>
@@ -59,14 +70,39 @@ class Build : NukeBuild
                 .LogInfo("Site published");
         });
 
+    Target PublishGitHubRelease => td => td
+        .OnlyWhenStatic(() => Repository.IsOnMainOrMasterBranch())
+        .Requires(() => GitHubApiKey)
+        .Executes(async () =>
+        {
+            await Solution.Projects
+                .TryFirst(x => x.GetOutputType().Contains("Exe", StringComparison.InvariantCultureIgnoreCase))
+                .ToResult("Could not find the executable project")
+                .Bind(project =>
+                {
+                    return new DeploymentBuilder(Actions, project)
+                        .ForLinux(Options())
+                        .ForWindows()
+                        .ForAndroid(Base64Keystore, AndroidSigningKeyAlias, AndroidSigningKeyPass, AndroidSigningStorePass)
+                        .Build()
+                        .Bind(paths => Actions.CreateGitHubRelease(GitHubApiKey, paths.ToArray()));
+                })
+                .TapError(err => throw new ApplicationException(err));
+        });
+
     Target Publish => td => td
-        .DependsOn(PublishNugetPackages, PublishSite);
+        .DependsOn(PublishNugetPackages, PublishSite, PublishGitHubRelease);
 
     IEnumerable<string> PackableProjects =>
         Solution.AllProjects
             .Where(x => x.GetProperty<bool>("IsPackable"))
             .Where(x => !(x.Path.ToString().Contains("Test", StringComparison.InvariantCultureIgnoreCase) || x.Path.ToString().Contains("Sample", StringComparison.InvariantCultureIgnoreCase)))
             .Select(x => x.Path.ToString());
+
+    protected override void OnBuildInitialized()
+    {
+        Actions = new Actions(Solution, Repository, RootDirectory, GitVersion, Configuration);
+    }
 
     public static int Main() => Execute<Build>(x => x.Publish);
 
@@ -77,5 +113,36 @@ class Build : NukeBuild
             .Output
             .Any(line => line.Text.Contains("wasm-tools"));
         return result;
+    }
+
+    Options Options()
+    {
+        IEnumerable<AdditionalCategory> additionalCategories = [];
+
+        return new Options
+        {
+            MainCategory = MainCategory.Development,
+            AdditionalCategories = Maybe.From(additionalCategories),
+            Name = "Zafiro.Avalonia Toolkit Catalog",
+            Version = GitVersion.MajorMinorPatch,
+            Comment = "Catalog of controls and other utilities in Zafiro.Avalonia",
+            Id = "com.SuperJMN.Zafiro.Avalonia",
+            StartupWmClass = "Zafiro.Avalonia",
+            HomePage = new Uri("https://github.com/SuperJMN-Zafiro/Zafiro.Avalonia"),
+            Keywords = new List<string>
+            {
+                "Toolkit",
+                "Avalonia",
+                "UI",
+                "UI Framework",
+                "Cross-platform",
+                "Multiplatform",
+                "Productivity",
+                "Library",
+            },
+            License = "MIT",
+            //ScreenshotUrls = Maybe.From(screenShots),
+            Summary = "Shows a catalog of controls and other utilities in Zafiro.Avalonia",
+        };
     }
 }
