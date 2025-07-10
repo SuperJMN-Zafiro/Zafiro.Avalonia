97ba5a29ddfbb6beb733c53c40dabb5a27abf4ba
SuperJMN <superjmn@outlook.com>
2025-07-11 00:45:28 +0200

    Enhance ReactiveSelection with SelectAll and SelectFiltered commands for improved item selection

--- src/Zafiro.Avalonia/Misc/ReactiveSelection.cs
+++ src/Zafiro.Avalonia/Misc/ReactiveSelection.cs
@@ -1,7 +1,11 @@
+using System.Collections;
 using System.Collections.ObjectModel;
+using System.Reactive;
 using System.Reactive.Disposables;
 using Avalonia.Controls.Selection;
 using DynamicData;
+using DynamicData.Aggregation;
+using Zafiro.Reactive;
 
 namespace Zafiro.Avalonia.Misc;
 
@@ -9,30 +13,94 @@ public class ReactiveSelection<T, TKey> : IDisposable where T : notnull where TK
 {
     private readonly CompositeDisposable disposable = new();
 
-    public ReactiveSelection(Func<T, TKey> selector)
+    public ReactiveSelection(SelectionModel<T> selectionModel, Func<T, TKey> selector, Func<T, bool>? canBeSelected = null)
     {
-        var cache = new SourceCache<T, TKey>(selector)
+        var selectedItemsCache = new SourceCache<T, TKey>(selector)
             .DisposeWith(disposable);
         
-        SelectionModel = new SelectionModel<T>();
-        
+        SelectionModel = selectionModel;
+        CanBeSelected = canBeSelected ?? (_ => true);
+
         var obs = Observable
             .FromEventPattern<SelectionModelSelectionChangedEventArgs<T>>(handler => SelectionModel.SelectionChanged += handler, handler => SelectionModel.SelectionChanged -= handler);
 
         obs
-            .Do(pattern => Sync(pattern.EventArgs, cache))
+            .Do(pattern => Sync(pattern.EventArgs, selectedItemsCache))
             .Subscribe()
             .DisposeWith(disposable);
 
-        cache.Connect(suppressEmptyChangeSets: false)
+        selectedItemsCache.Connect(suppressEmptyChangeSets: false)
             .Bind(out var selectedItems)
             .Subscribe()
             .DisposeWith(disposable);
+        
+        var itemChanges = this.WhenAnyValue(x => x.SelectionModel.Source).Select(src => GetItemChanges(src)).Switch();
+        
+        var counts = itemChanges.Count().CombineLatest(selectedItemsCache.CountChanged, (totalCount, selectedCount) => (totalCount, selectedCount));
+        var filteredCounts = itemChanges.Filter(CanBeSelected).Count().CombineLatest(selectedItemsCache.CountChanged, (totalCount, selectedCount) => (totalCount, selectedCount));
+
+        SelectAllCommand = ReactiveCommand.Create(() => SelectAll(), counts.Select(x => x.totalCount > x.selectedCount && x.totalCount > 0));
+        SelectAllFilteredCommand = ReactiveCommand.Create(() => SelectFiltered(), filteredCounts.Select(x => x.totalCount > x.selectedCount && x.totalCount > 0));
+        ClearCommand = ReactiveCommand.Create(() => Clear(), counts.Select(x => x.selectedCount > 0));
 
         SelectedItems = selectedItems;
     }
 
+    public ReactiveCommand<Unit, Unit> ClearCommand { get; set; }
+
+    public ReactiveCommand<Unit, Unit> SelectAllFilteredCommand { get; }
+
+    private static IObservable<IChangeSet<T>> GetItemChanges(IEnumerable? src)
+    {
+        return src is IEnumerable<T> source ? source.ToObservableChangeSetIfPossible() : Enumerable.Empty<T>().AsObservableChangeSet();
+    }
+
+    public ReactiveCommand<Unit, Unit> SelectAllCommand { get; }
+
+    public void SelectAll()
+    {
+        if (SelectionModel.SingleSelect)
+        {
+            return;
+        }
+        
+        SelectionModel.SelectAll();
+    }
+    
+    public void SelectFiltered()
+    {
+        if (SelectionModel.SingleSelect)
+        {
+            return;
+        }
+
+        if (SelectionModel.Source is null)
+        {
+            return;
+        }
+
+        var selectedIndices = ItemList?.Where(CanBeSelected).Select(x => ItemList.IndexOf(x));
+        
+        if (selectedIndices is null)
+        {
+            return;
+        }
+
+        foreach (var selectedItem in selectedIndices)
+        {
+            SelectionModel.Select(selectedItem);   
+        }
+    }
+    
+    public IList<T>? ItemList => SelectionModel.Source?.Cast<T>().ToList(); 
+    
+    public void Clear()
+    {
+        SelectionModel.Clear();
+    }
+
     public SelectionModel<T> SelectionModel { get; }
+    public Func<T, bool> CanBeSelected { get; }
 
     public ReadOnlyObservableCollection<T> SelectedItems { get; }
 
