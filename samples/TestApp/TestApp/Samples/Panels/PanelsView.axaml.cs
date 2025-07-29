2a0e8703e7877dc48b001e4aa9e8b5c6d0780aa8
SuperJMN <superjmn@outlook.com>
2025-07-29 17:32:56 +0200

    Add ResponsiveGridPanel and PanelsView for improved layout management

---
+++ samples/TestApp/TestApp/Samples/Panels/PanelsView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace TestApp.Samples.Panels;
+
+public partial class PanelsView : UserControl
+{
+    public PanelsView()
+    {
+        InitializeComponent();
+    }
+}
--- src/Zafiro.Avalonia/Controls/CardGrid.axaml.cs
+++ src/Zafiro.Avalonia/Controls/CardGrid.axaml.cs
@@ -10,14 +10,26 @@ public class CardGrid : TemplatedControl
     public static readonly StyledProperty<IEnumerable> ItemsSourceProperty =
         AvaloniaProperty.Register<CardGrid, IEnumerable>(nameof(ItemsSource));
 
+    public static readonly StyledProperty<IDataTemplate> ItemTemplateProperty = AvaloniaProperty.Register<CardGrid, IDataTemplate>(
+        nameof(ItemTemplate));
+
+    public static readonly StyledProperty<double> RowSpacingProperty = AvaloniaProperty.Register<CardGrid, double>(
+        nameof(RowSpacing));
+
+    public static readonly StyledProperty<double> ColumnSpacingProperty = AvaloniaProperty.Register<CardGrid, double>(
+        nameof(ColumnSpacing));
+
+    public static readonly StyledProperty<ControlTheme> ItemContainerThemeProperty = AvaloniaProperty.Register<CardGrid, ControlTheme>(
+        nameof(ItemContainerTheme));
+
+    public static readonly StyledProperty<double> MinColumnWidthProperty = AvaloniaProperty.Register<CardGrid, double>(
+        nameof(MinColumnWidth), 300d);
+
     public IEnumerable ItemsSource
     {
         get => GetValue(ItemsSourceProperty);
         set => SetValue(ItemsSourceProperty, value);
     }
-
-    public static readonly StyledProperty<IDataTemplate> ItemTemplateProperty = AvaloniaProperty.Register<CardGrid, IDataTemplate>(
-        nameof(ItemTemplate));
 
     public IDataTemplate ItemTemplate
     {
@@ -25,17 +37,11 @@ public class CardGrid : TemplatedControl
         set => SetValue(ItemTemplateProperty, value);
     }
 
-    public static readonly StyledProperty<double> RowSpacingProperty = AvaloniaProperty.Register<CardGrid, double>(
-        nameof(RowSpacing));
-
     public double RowSpacing
     {
         get => GetValue(RowSpacingProperty);
         set => SetValue(RowSpacingProperty, value);
     }
-
-    public static readonly StyledProperty<double> ColumnSpacingProperty = AvaloniaProperty.Register<CardGrid, double>(
-        nameof(ColumnSpacing));
 
     public double ColumnSpacing
     {
@@ -43,30 +49,15 @@ public class CardGrid : TemplatedControl
         set => SetValue(ColumnSpacingProperty, value);
     }
 
-    public static readonly StyledProperty<ControlTheme> ItemContainerThemeProperty = AvaloniaProperty.Register<CardGrid, ControlTheme>(
-        nameof(ItemContainerTheme));
-
     public ControlTheme ItemContainerTheme
     {
         get => GetValue(ItemContainerThemeProperty);
         set => SetValue(ItemContainerThemeProperty, value);
     }
 
-    public static readonly StyledProperty<double> MinColumnWidthProperty = AvaloniaProperty.Register<CardGrid, double>(
-        nameof(MinColumnWidth), 300d);
-
     public double MinColumnWidth
     {
         get => GetValue(MinColumnWidthProperty);
         set => SetValue(MinColumnWidthProperty, value);
-    }
-
-    public static readonly StyledProperty<double> MaxColumnWidthProperty = AvaloniaProperty.Register<CardGrid, double>(
-        nameof(MaxColumnWidth), 500d);
-
-    public double MaxColumnWidth
-    {
-        get => GetValue(MaxColumnWidthProperty);
-        set => SetValue(MaxColumnWidthProperty, value);
     }
 }
