4aedc91fe21af986916b8bbcb6985ae947801f64
José Manuel Nieto <superjmn@outlook.com>
2025-08-05 12:11:01 +0200

    Add ReactiveIconButton control (#98)

---
+++ samples/TestApp/TestApp/Samples/ReactiveIconButton/ReactiveIconButtonViewModel.cs
@@ -0,0 +1,6 @@
+using Zafiro.UI.Shell.Utils;
+
+namespace TestApp.Samples.ReactiveIconButton;
+
+[Section(icon: "mdi-button-cursor", name: "Reactive Icon Button", sortIndex: 8)]
+public class ReactiveIconButtonViewModel;
