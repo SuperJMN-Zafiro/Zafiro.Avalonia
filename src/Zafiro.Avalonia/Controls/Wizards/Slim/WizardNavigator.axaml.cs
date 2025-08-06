5ba26176af8369a0aa8740bbad40274b89fa663d
José Manuel Nieto <superjmn@outlook.com>
2025-08-07 00:16:28 +0200

    Guard cancel button during progress or completion

--- src/Zafiro.Avalonia/Controls/Wizards/Slim/WizardNavigator.axaml.cs
+++ src/Zafiro.Avalonia/Controls/Wizards/Slim/WizardNavigator.axaml.cs
@@ -1,3 +1,7 @@
+using System;
+using System.ComponentModel;
+using System.Reactive.Linq;
+using System.Windows.Input;
 using Avalonia.Controls.Primitives;
 using Zafiro.UI.Commands;
 using Zafiro.UI.Wizards.Slim;
@@ -12,6 +16,12 @@ public class WizardNavigator : TemplatedControl
     public static readonly StyledProperty<IEnhancedCommand> CancelProperty = AvaloniaProperty.Register<WizardNavigator, IEnhancedCommand>(
         nameof(Cancel));
 
+    public static readonly DirectProperty<WizardNavigator, bool> CanCancelProperty =
+        AvaloniaProperty.RegisterDirect<WizardNavigator, bool>(nameof(CanCancel), o => o.CanCancel);
+
+    private IDisposable? canCancelSubscription;
+    private bool canCancel;
+
     public ISlimWizard Wizard
     {
         get => GetValue(WizardProperty);
@@ -22,5 +32,51 @@ public class WizardNavigator : TemplatedControl
     {
         get => GetValue(CancelProperty);
         set => SetValue(CancelProperty, value);
+    }
+
+    public bool CanCancel
+    {
+        get => canCancel;
+        private set => SetAndRaise(CanCancelProperty, ref canCancel, value);
+    }
+
+    static WizardNavigator()
+    {
+        WizardProperty.Changed.Subscribe(args =>
+        {
+            if (args.Sender is WizardNavigator navigator)
+            {
+                navigator.SetupCanCancel(args.NewValue.GetValueOrDefault<ISlimWizard>());
+            }
+        });
+    }
+
+    private void SetupCanCancel(ISlimWizard wizard)
+    {
+        canCancelSubscription?.Dispose();
+        if (wizard is null)
+        {
+            CanCancel = false;
+            return;
+        }
+
+        var currentPage = Observable.FromEventPattern<PropertyChangedEventHandler, PropertyChangedEventArgs>(
+                h => ((INotifyPropertyChanged)wizard).PropertyChanged += h,
+                h => ((INotifyPropertyChanged)wizard).PropertyChanged -= h)
+            .Where(e => e.EventArgs.PropertyName == nameof(ISlimWizard.CurrentPage))
+            .Select(_ => wizard.CurrentPage)
+            .StartWith(wizard.CurrentPage);
+
+        var backCanExecute = ((IReactiveCommand)wizard.Back).CanExecute
+            .StartWith(((ICommand)wizard.Back).CanExecute(null));
+
+        var nextExecuting = ((IReactiveCommand)wizard.Next).IsExecuting.StartWith(false);
+
+        var onCompletion = currentPage.CombineLatest(backCanExecute, (page, canBack) =>
+            page.Index == wizard.TotalPages - 1 && !canBack);
+
+        canCancelSubscription = onCompletion
+            .CombineLatest(nextExecuting, (completion, executing) => !completion && !executing)
+            .Subscribe(value => CanCancel = value);
     }
 }
