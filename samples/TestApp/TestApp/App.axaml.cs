75cc70baf9c59e29fc6d10776e92c1e811052873
SuperJMN <superjmn@outlook.com>
2025-09-05 15:20:32 +0200

    Cleanup: revert App.axaml.cs to original Connect-based initialization (remove Android-specific eager setup and diagnostic hooks)

--- samples/TestApp/TestApp/App.axaml.cs
+++ samples/TestApp/TestApp/App.axaml.cs
@@ -21,26 +21,7 @@ public class App : Application
             .Register<FontAwesomeIconProvider>()
             .Register<MaterialDesignIconProvider>();
 
-        // On Android, assign MainView and DataContext immediately to avoid a blank screen if Loaded is delayed
-        if (ApplicationLifetime is global::Avalonia.Controls.ApplicationLifetimes.ISingleViewApplicationLifetime singleView)
-        {
-            var mainView = new MainView();
-            singleView.MainView = mainView;
-
-            // Log assembly resolution requests to identify missing assemblies under AOT
-            System.AppDomain.CurrentDomain.AssemblyResolve += (sender, args) =>
-            {
-                try
-                {
-                    var an = new System.Reflection.AssemblyName(args.Name);
-                    System.Console.WriteLine($"[TestApp] AssemblyResolve requested: {an.Name}");
-                }
-                catch { }
-                return null;
-            };
-
-            this.Connect(() => new MainView(), view => CompositionRoot.Create(), () => new MainWindow());
-        }
+        this.Connect(() => new MainView(), view => CompositionRoot.Create(), () => new MainWindow());
 
         base.OnFrameworkInitializationCompleted();
     }
