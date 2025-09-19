09e324ff73faac6e1406c129fcc4c6e7f6ab1542
SuperJMN <superjmn@outlook.com>
2025-09-19 19:54:42 +0200

    Refactor UI components for consistency and improved icon handling

--- src/Zafiro.Avalonia/Controls/IconOptions.cs
+++ src/Zafiro.Avalonia/Controls/IconOptions.cs
@@ -2,8 +2,8 @@ namespace Zafiro.Avalonia.Controls;
 
 public class IconOptions
 {
-    public static readonly AttachedProperty<double> SizeProperty = AvaloniaProperty.RegisterAttached<IconOptions, Visual, double>("Size", inherits: true, defaultValue: 32);
+    public static readonly AttachedProperty<double> SizeProperty = AvaloniaProperty.RegisterAttached<IconOptions, AvaloniaObject, double>("Size", inherits: true, defaultValue: 32);
 
-    public static void SetSize(Visual obj, double value) => obj.SetValue(SizeProperty, value);
-    public static double GetSize(Visual obj) => obj.GetValue(SizeProperty);
+    public static void SetSize(AvaloniaObject obj, double value) => obj.SetValue(SizeProperty, value);
+    public static double GetSize(AvaloniaObject obj) => obj.GetValue(SizeProperty);
 }
